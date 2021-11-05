7705a67836ef9b809b160d715755b16d1b2ef51c
DDSH0T <93758800+DDSH0T@users.noreply.github.com>
2021-11-05 12:35:02 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -30,12 +30,11 @@ Bot Owner Only
 ABOUT = """
 ➥ Name :  Auto Filter V3
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : [Xx-DΞΛDSH0T-xX
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+➥ My Group : [Click](https://t.me/D_S_M_A_L_A_1)
 """
